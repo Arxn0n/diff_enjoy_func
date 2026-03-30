5f6b65ca390042b4cf3fe915778302876f9b615b
Arxn0n <143648018+Arxn0n@users.noreply.github.com>
2026-03-30 11:57:31 +0300

    Create search_median.py

---
+++ search_median.py
@@ -0,0 +1,8 @@
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged = sorted(nums1 + nums2)
+        n = len(merged)
+        if n % 2 == 1:
+            median = merged[n // 2]
+        else:
+            median = (merged[n // 2 - 1] + merged[n // 2]) / 2
+        return median
